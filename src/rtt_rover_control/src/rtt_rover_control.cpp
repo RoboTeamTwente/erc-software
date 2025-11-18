88a5b72989affb167383ad54b2b868862c7d8600
jarkad <jarkad@jarkad.net.eu.org>
2025-11-18 11:02:48 +0100

    feat: create empty rtt_rover_control package

---
+++ src/rtt_rover_control/src/rtt_rover_control.cpp
@@ -0,0 +1,14 @@
+#include "rtt_rover_control/rtt_rover_control.hpp"
+
+namespace rtt_rover_control
+{
+
+RttRoverControl::RttRoverControl()
+{
+}
+
+RttRoverControl::~RttRoverControl()
+{
+}
+
+}  // namespace rtt_rover_control
